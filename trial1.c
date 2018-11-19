a987762657f8fe262c16f2cc2bb164efd1feb2c6
KshitijLakhani <klakhani@ucdavis.edu>
2018-11-19 15:24:25 -0800

    Initial commit

---
+++ trial1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+
+int ip;
+printf("Enter the number you want to check for odd / even");
+scanf("%d",&ip);
+if(ip%2 == 0){
+printf("%d is divisible by 2",ip);
+}
+else{
+printf("%d is not divisible by 2",ip);
+}
---
+++ trial1.py
@@ -0,0 +1,6 @@
+ip = input("Enter a number to see if it is even or odd")
+if ip % 2 == 0:
+	print("{} is even".format(ip))
+else:
+	print("{} is odd".format(ip))
+
