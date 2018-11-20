2b29d25a292ae8e557409fd438e7fbdb72628544
KshitijLakhani <klakhani@ucdavis.edu>
2018-11-19 16:01:16 -0800

    Changed trial1.c

--- trial1.c
+++ trial1.c
@@ -2,11 +2,15 @@
 #include <conio.h>
 
 int ip;
+int temp = 0;
 printf("Enter the number you want to check for odd / even");
 scanf("%d",&ip);
-if(ip%2 == 0){
-printf("%d is divisible by 2",ip);
+if(ip%2 == 0)
+temp = 1;
 }
-else{
-printf("%d is not divisible by 2",ip);
+
+if(temp == 0){
+printf("Number is odd")
 }
+else
+printf("Number is even")
